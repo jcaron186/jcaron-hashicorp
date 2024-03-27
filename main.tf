ddcb39cf35e14593354d75d4b220320023c71056
Josh Caron <100725198+jcaron186@users.noreply.github.com>
2024-03-27 07:59:37 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ locals {
 }
 
 
-(* Creates Vault Okta OIDC Config. *)
+// Creates Vault Okta OIDC Config
 resource "vault_jwt_auth_backend" "okta-oidc" {
   path = "okta-oidc"
   oidc_discovery_url = local.oidc_discovery_url
@@ -16,7 +16,7 @@ resource "vault_jwt_auth_backend" "okta-oidc" {
   default_role       = "okta-role"
 }
 
-(* Creates Vault Okta OIDC Role. *)
+// Creates Vault Okta OIDC Role
 resource "vault_jwt_auth_backend_role" "okta-role" {
   backend               = vault_jwt_auth_backend.okta-oidc.path
   bound_audiences       = [ "" ]
@@ -29,7 +29,7 @@ resource "vault_jwt_auth_backend_role" "okta-role" {
   verbose_oidc_logging  = true
 }
 
-(* Creates an external Identity Group for Vault Okta OIDC Admins. *)
+// Creates an external Identity Group for Vault Okta OIDC Admins
 resource "vault_identity_group" "vault-admins" {
   name     = local.vault_group_name
   type     = "external"
@@ -39,19 +39,19 @@ resource "vault_identity_group" "vault-admins" {
   }
 }
 
-(* Creates an external Identity Group Alias for Vault Okta OIDC Group Admins.  *)
+// Creates an external Identity Group Alias for Vault Okta OIDC Group Admins
 resource "vault_identity_group_alias" "vault-admins-alias" {
   name           = local.okta_group_name
   mount_accessor = vault_jwt_auth_backend.okta-oidc.accessor
   canonical_id   = vault_identity_group.vault-admins.id
 }
 
-(* Creates child namespace under admin. *)
+// Creates child namespace under admin.
 resource "vault_namespace" "ns1" {
      path = "ns1"
 }
   
-(* Vault INTERNAL group that exists in the dedicated Namespace and grants permissions to that Namespace. *)
+// Vault INTERNAL group that exists in the dedicated Namespace and grants permissions to that Namespace
 resource "vault_identity_group" "ns1-group" {
    namespace                 = vault_namespace.ns1.path
    name                      = "ns1-group"
@@ -60,7 +60,7 @@ resource "vault_identity_group" "ns1-group" {
    policies                  = ["default", "ns1-root"]
 }
 
-(* This resource will manage the Vault identity groups that will automatically be added to this group. *)
+// This resource will manage the Vault identity groups that will automatically be added to this group
 resource "vault_identity_group_member_group_ids" "ns1-group-members" {
    namespace        = vault_namespace.ns1.path
    exclusive        = true
